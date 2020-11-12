8ba8465dcac892eb772b958084713fec0ceb101b
MultiDream <luka.derado@gmail.com>
2020-11-11 23:20:09 -0500

    Submitted my work on a waking sight rule script
    Made some work toward a waking sight ruleset object, but without syntax checking can't go much further.

---
+++ Assets/Scripts/WakingSight/WakingSightRules.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WakingSightRules
+{
+    public static Dictionary<WakingSightMode, bool> isEnabled; // = DefaultRules();
+
+    public static Dictionary<WakingSightMode, bool> DefaultRules()
+    {
+        Dictionary<WakingSightMode, bool> rules = new Dictionary<WakingSightMode, bool>();
+        foreach (WakingSightMode mode in enum.GetValues(WakingSightMode))
+        {
+            rules.Add(mode, false);
+        }
+
+        rules.Add(WakingSightMode.Default);
+
+        return rules;
+    }
+}
+
+public enum WakingSightMode : int
+{
+    Default,
+    One,
+    Two,
+    Three,
+    Four,
+    Final
+}
